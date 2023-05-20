98a4290cb10953f9db4985f2ffa9fcaf258ee671
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-05-20 21:13:21 +0530

    modify: updated root index file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import { ProductsProvider } from "./core/contexts/products-context/ProductsContext";
 
 // Call make Server
 makeServer();
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
