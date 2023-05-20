8a6a52d15b201e5942863f944552a96795c4af49
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-05-20 21:14:16 +0530

    feat: integrated API to render products list

--- src/components/features/products-listing/ProductsListing.jsx
+++ src/components/features/products-listing/ProductsListing.jsx
@@ -1,5 +1,29 @@
+import { useProducts } from "../../../core/contexts/products-context/ProductsContext";
+
 const ProductsListing = () => {
-  return <h2>ProductsListing</h2>;
+  const { products } = useProducts();
+
+  return (
+    <div style={{ display: "flex", margin: "1rem auto", justifyContent:'center' }}>
+      {products.length > 0 &&
+        products.map(({ id, title, author, price, categoryName }) => (
+          <div
+            key={id}
+            style={{
+              border: "1px solid",
+              height: "300px",
+              width: "200px",
+              margin: "1rem",
+            }}
+          >
+            <h3>{title}</h3>
+            <p>{author}</p>
+            <p>{price}</p>
+            <p>{categoryName}</p>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default ProductsListing;
