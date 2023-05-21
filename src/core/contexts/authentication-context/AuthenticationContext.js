f484f40256f4fe2b29f0ba377cedbe39880b0506
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-05-21 14:19:48 +0530

    feat:log out functionality

--- src/core/contexts/authentication-context/AuthenticationContext.js
+++ src/core/contexts/authentication-context/AuthenticationContext.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { createContext, useContext, useState } from "react";
+import { useProducts } from "../products-context/ProductsContext";
 
 export const AuthenticationContext = createContext();
 
@@ -8,8 +9,25 @@ export const AuthenticationProvider = ({ children }) => {
     token: JSON.parse(localStorage.getItem("token")),
     user: JSON.parse(localStorage.getItem("user")),
   });
+  const { dispatch } = useProducts();
 
-  const loginUser = async (body) => {
+  const syncUserDetailsWithCartAndWishlist = (token, user) => {
+    setLoggedInUserDetails((prev) => ({
+      ...prev,
+      token,
+      user,
+    }));
+    dispatch({ type: "UPDATE_CART_AND_WISHLIST", payload: user });
+  };
+
+  const logOutUser = () => {
+    setLoggedInUserDetails((prev) => ({ ...prev, token: "", user: null }));
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    dispatch({ type: "LOG_OUT" });
+  };
+
+  const logInUser = async (body) => {
     try {
       const response = await axios.post("/api/auth/login", body);
       const {
@@ -19,19 +37,33 @@ export const AuthenticationProvider = ({ children }) => {
       if (status === 200) {
         localStorage.setItem("token", JSON.stringify(encodedToken));
         localStorage.setItem("user", JSON.stringify(foundUser));
-        setLoggedInUserDetails((prev) => ({
-          ...prev,
-          token: encodedToken,
-          user: foundUser,
-        }));
+        syncUserDetailsWithCartAndWishlist(encodedToken, foundUser);
       }
     } catch (error) {
       console.error(error);
     }
   };
+
+  const signUpUser = async (body) => {
+    try {
+      const response = await axios.post("/api/auth/signup", body);
+      const {
+        data: { createdUser, encodedToken },
+        status,
+      } = response;
+      if (status === 201) {
+        localStorage.setItem("token", JSON.stringify(encodedToken));
+        localStorage.setItem("user", JSON.stringify(createdUser));
+        syncUserDetailsWithCartAndWishlist(encodedToken, createdUser);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <AuthenticationContext.Provider
-      value={{ ...loggedInUserDetails, loginUser }}
+      value={{ ...loggedInUserDetails, logInUser, signUpUser, logOutUser }}
     >
       {children}
     </AuthenticationContext.Provider>
