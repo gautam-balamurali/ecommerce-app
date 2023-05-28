cad6220de7569c215187d334bd49a31f9fa5043f
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-05-28 13:11:03 +0530

    style: header styling phase 1

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
+import "./Header.css";
+
 import Navbar from "../navbar/Navbar";
+import SearchBar from "../search-bar/SearchBar";
 
 const Header = () => {
   return (
-    <>
-      <Link className="title-link" to={"/"}>
-        <h1>Sochenge</h1>
-      </Link>
-      <Navbar />
-    </>
+    <header className="header">
+      <div className="header-content app-title">
+        <Link className="nav-link" to={"/"}>
+          <h2>Sochenge</h2>
+        </Link>
+      </div>
+      <div className="header-content">
+        <SearchBar />
+      </div>
+      <div className="header-content">
+        <Navbar />
+      </div>
+    </header>
   );
 };
 
